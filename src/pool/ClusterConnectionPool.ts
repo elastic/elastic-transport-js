63d3308983fb3d0f5d9cc39786a0c3c093b4265e
delvedor <tommydelved@gmail.com>
2021-03-13 14:34:16 +0100

    Fixed resurrect logic

--- src/pool/ClusterConnectionPool.ts
+++ src/pool/ClusterConnectionPool.ts
@@ -181,7 +181,14 @@ export default class ClusterConnectionPool extends BaseConnectionPool {
           })
         })
         .catch((err: Error) => {
-          this.diagnostic.emit('resurrect', err, null)
+          this.markDead(connection)
+          this.diagnostic.emit('resurrect', err, {
+            strategy: 'ping',
+            name: opts.name,
+            request: { id: opts.requestId },
+            isAlive: false,
+            connection
+          })
         })
     // optimistic strategy
     } else {
