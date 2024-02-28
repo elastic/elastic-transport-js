af8ff6132bdafcf296daf734a02cf0e80f7a6125
Breno Alves <39974954+breno-alves@users.noreply.github.com>
2024-02-28 19:11:15 -0300

    fix: increase default value of max event listeners (#81)

--- src/connection/UndiciConnection.ts
+++ src/connection/UndiciConnection.ts
@@ -68,6 +68,7 @@ export default class Connection extends BaseConnection {
     }
 
     this[kEmitter] = new EventEmitter()
+    this[kEmitter].setMaxListeners(this.maxEventListeners)
     const undiciOptions: Pool.Options = {
       keepAliveTimeout: 600e3,
       keepAliveMaxTimeout: 600e3,
