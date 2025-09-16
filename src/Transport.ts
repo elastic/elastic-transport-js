f3dddc685ec357fafb7befd86a055b4a86c4e094
Josh Mock <joshua.mock@elastic.co>
2025-09-16 11:26:25 -0500

    Add support for accepted params metadata

--- src/Transport.ts
+++ src/Transport.ts
@@ -122,6 +122,7 @@ export interface TransportOptions {
 export interface TransportRequestMetadata {
   name: string
   pathParts?: Record<string, any>
+  acceptedParams?: string[]
 }
 
 export interface TransportRequestParams {
