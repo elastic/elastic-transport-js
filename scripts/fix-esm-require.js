81ca0f8184ae55477d94122129aed9c2e2c5c658
Josh Mock <joshua.mock@elastic.co>
2026-01-20 14:45:28 -0500

    Clean up SPDX license to pass check

--- scripts/fix-esm-require.js
+++ scripts/fix-esm-require.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 /*
  * Copyright Elasticsearch B.V. and contributors
  * SPDX-License-Identifier: Apache-2.0
@@ -23,20 +22,20 @@ if (!content.includes('createRequire') && content.includes("require('../package.
   // Find the last import statement
   const lastImportIndex = content.lastIndexOf('import ')
   const newlineAfterImport = content.indexOf('\n', lastImportIndex)
-  
+
   if (newlineAfterImport !== -1) {
     const beforeImport = content.substring(0, newlineAfterImport + 1)
     const afterImport = content.substring(newlineAfterImport + 1)
-    
+
     content = beforeImport + "import { createRequire } from 'node:module';\nconst require = createRequire(import.meta.url);\n" + afterImport
   }
-  
+
   // Fix the path to package.json - from lib/esm, it's ../../package.json
   content = content.replace(
     /require\(['"]\.\.\/package\.json['"]\)/g,
     "require('../../package.json')"
   )
-  
+
   fs.writeFileSync(transportPath, content, 'utf8')
   console.log(`Fixed package.json loading in ${transportPath}`)
 } else {
